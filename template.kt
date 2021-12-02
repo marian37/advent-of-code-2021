9e26abb55accd3c2b02293602a648d413072f400
marian37 <marian.opiela@vacuumlabs.com>
2021-12-02 21:44:49 +0100

    Initial commit
    - Setup template and .gitignore

---
+++ template.kt
@@ -0,0 +1,25 @@
+import java.io.File
+
+fun readInput(name: String) = File("$name.txt").readLines()
+
+fun main() {
+  fun part1(input: List<String>): Int {
+    return input.size
+  }
+
+  fun part2(input: List<String>): Int {
+    return input.size
+  }
+
+  // test if implementation meets criteria from the description, like:
+  val testInput = readInput("00_test")
+  check(part1(testInput) == 1)
+
+  val input = readInput("00")
+  println(part1(input))
+  println(part2(input))
+}
+
+// command:
+// kotlinc -include-runtime -d template.jar template.kt
+// java -jar template.jar
